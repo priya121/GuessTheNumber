b65be4771d2fc9e88c48b93f9caeb7fdca3ad5cf
priya121 <priya121@github.com>
2015-10-20 15:13:33 +0100

    Added a game loop and using FakeIO interface in main game and changed tests.

--- src/GuessTheNumberTest.java
+++ src/GuessTheNumberTest.java
@@ -8,24 +8,23 @@ public class GuessTheNumberTest {
 
     @Test
     public void checksIfNumberMatches() {
+        FakeIO io = new FakeIO(asList(1, 2));
+        newGame = new GuessTheNumber(io, 2);
         assertTrue(newGame.isMatch(2));
     }
 
     @Test
     public void checksNumberDoesNotMatch() {
+        FakeIO io = new FakeIO(asList(1, 2));
+        newGame = new GuessTheNumber(io, 2);
         assertFalse(newGame.isMatch(4));
-    }
-
-    @Test
-    public void usesFakeIOToGetInput() {
-        FakeIO io = new FakeIO(asList(1));
-        assertEquals(false, newGame.gameLoop());
     }
 
     @Test
     public void usesFakeIOToGetListInput() {
         FakeIO io = new FakeIO(asList(1, 2));
-        assertEquals(false, newGame.gameLoop());
+        newGame = new GuessTheNumber(io, 2);
+        assertEquals(true, newGame.gameLoop());
     }
 
     @Test
@@ -35,4 +34,3 @@ public class GuessTheNumberTest {
         assertEquals(true, newGame.gameLoop());
     }
 }
-
